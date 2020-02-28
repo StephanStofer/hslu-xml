e0174ef9fddf9b8539ed6740475021ed3119bf1a
mauho <maurizio.hostettler@gmail.com>
2020-02-29 00:25:32 +0100

    add transform with id in order to pass a parameter to the xsl

--- frontend/php/transform.php
+++ frontend/php/transform.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 function transformXml($xml_path, $xsl_path)
 {
     $xml = new DOMDocument();
@@ -12,6 +10,20 @@ function transformXml($xml_path, $xsl_path)
     $processor = new XSLTProcessor();
     $processor->importStylesheet($xsl);
 
+    print($processor->transformToXml($xml));
+}
+
+function transformXmlId($xml_path, $xsl_path, $eventid)
+{
+    $xml = new DOMDocument();
+    $xml->load($xml_path);
+    $xsl = new DOMDocument();
+    $xsl->load($xsl_path);
+
+    $processor = new XSLTProcessor();
+    $processor->importStylesheet($xsl);
+	
+	$processor->setParameter('', 'eventid', $eventid);
     print($processor->transformToXml($xml));
 }
 

9590e07e0da38e6e80b3dc6ce6ef100007447c08
Julien Grueter <julien.grueter@gmail.com>
2020-02-29 11:11:59 +0100

    fix typo for xsl

--- singleevent.php
+++ singleevent.php
@@ -3,7 +3,7 @@ require_once 'frontend/php/transform.php';
 
 ## singleevent should be called with an event ID in order to display the proper event.
 if (isset($_GET['eventid'])){
-	transformXmlId('database/events.xml', 'frontend/xslt/singleevent.xsl', $_GET['eventid']);
+	transformXmlId('database/events.xml', 'frontend/xslt/singleEvent.xsl', $_GET['eventid']);
 } else {
 	
 ##text this event does not exist anymore

eef5f413d24bea2880b2d7d48049dde4614b368d
Maurizio Hostettler <maurizio.hostettler@gmail.com>
2020-03-02 23:31:32 +0100

    add uniqueID

--- frontend/php/insert.php
+++ frontend/php/insert.php
@@ -1,29 +1,28 @@
  <?php
  error_reporting(E_ALL);
 if(isset($_POST['send'])){
-#minus submit, fname, lname, eventid
-	$numOptions = count($_POST);
-	
-	$eventid = $_POST['eventid'];
-		
-	## SOUT
-	echo "number of params: ",$numOptions,"<br/><br/>";/*
-	foreach($_POST AS $key => $value) { echo $key," : ", $value, "<br/>";}
-		*/
 	$xml = '../../database/events.xml';
-
 	$dom = new DomDocument('1.0', 'UTF-8');
 	$dom->load($xml);
 	
-	// define id attributes
+	// define id attributes - needed to actually add attributes
 	foreach($dom->getElementsByTagName('event') as $event)
 		$event->setIdAttribute('id',true);
+ 
+	$eventid = $_POST['eventid'];
  
 	$event_root = $dom->getElementById($eventid);
 	$participants = $event_root->getElementsByTagName('participants')->item(0);
 	
 	$newPerson = $dom->createElement("person");
 	
+	//Unique registration ID
+	$regId = uniqid();
+	$newAttribute = $dom->createAttribute("id");
+	$newAttribute->value=($regId);
+	$newPerson->appendChild($newAttribute);
+	
+	//Add every Post item as attribute exept those in the array :-)
 	foreach($_POST AS $key => $value) {
 		if (!in_array($key, array("eventid", "send"))){
 			echo $key," : ", $value, "<br/>";
@@ -34,6 +33,9 @@ if(isset($_POST['send'])){
 	}
 	$participants->appendChild($newPerson);
 	$dom->save($xml);
+	
+	//check wether the generated $xml is valid against person.xsd
+	
 	
 	echo "saved in xml";
 	
